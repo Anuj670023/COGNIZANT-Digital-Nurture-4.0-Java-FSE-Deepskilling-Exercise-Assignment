35ac3c271897eab041de7d0bcf33b19490472f7a
ANUJ MAURYA <149321171+Anuj670023@users.noreply.github.com>
2025-06-22 18:28:33 +0530

    Update ForecastRequest.java

--- DN-4.0_JAVA_FSE_6385773_Solutions/WEEK 1/Data structures and Algorithms/Exercise 7 Financial Forecasting/Code/ForecastRequest.java
+++ DN-4.0_JAVA_FSE_6385773_Solutions/WEEK 1/Data structures and Algorithms/Exercise 7 Financial Forecasting/Code/ForecastRequest.java
@@ -1,7 +1,3 @@
-// How to Use:
-// Create a forecast request for Rs. 10,000 at 5% annual growth for 5 years
-// ForecastRequest request = new ForecastRequest(10000, 0.05, 5);
-
 public class ForecastRequest {
     private final double initialValue;
     private final double annualRate;
